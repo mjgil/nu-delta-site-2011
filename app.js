e03a287b8fbb4ba07c2aa0565739ada2cdf5dc9f
Ubuntu <ubuntu@ip-10-169-64-214.ec2.internal>
2015-04-08 01:41:16 +0000

    update app/package.json

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express');
+var compression = require('compression');
 var app = express();
 
-app.use(express.compress());
+app.use(compression());
 app.use(express.static(__dirname));
 
 app.listen(3003);
